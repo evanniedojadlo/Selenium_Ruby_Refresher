efdb5b82bd198003b41767d30bb8deea17e42e60
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-04-15 23:46:42 -0500

    Adding a few tests which will be used for reference.

---
+++ ignore_this_test.rb
@@ -0,0 +1,17 @@
+require 'selenium-webdriver'
+
+
+#Tests/Specifications in rspec always begin with it
+#@webdriver is a selenium object
+
+
+page_title = @driver.title #common example of storing an element, @driver is the variable we will use to interact with selenium
+
+
+#explicit wait example also showing one argument
+
+def wait_for(seconds=10)
+	Selenium::WebDriver::Wait.new(:timeout => seconds).until {yield}
+end
+
+
