cbb9a04dd5c0e780e4f62cafeb05d20c6c519444
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-04-17 00:04:22 -0500

    Added basic login test using Mozilla Firefox

---
+++ login_spec.rb
@@ -0,0 +1,22 @@
+require 'selenium-webdriver'
+
+describe 'Login' do
+
+	before(:each) do
+		@driver = Selenium::WebDriver.for :firefox
+	end
+
+	after(:each) do
+		@driver.quit
+	end
+
+	it 'succeeded' do
+		@driver.get 'http://the-internet.herokuapp.com/login'
+		@driver.find_element(id: 'username').send_keys 'tomsmith'
+		@driver.find_element(id: 'password').send_keys 'SuperSecretPassword!'
+		@driver.find_element(css: 'button').click
+		#@driver.find_element(id: 'login').click note that we could also use this option
+	end
+
+
+end
