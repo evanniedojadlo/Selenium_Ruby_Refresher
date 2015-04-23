ab0ca65ef02d954715ad68079f6317b00bd6aca5
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-04-23 00:50:26 -0500

    Updated the login spec using page objects and DRY

--- login_spec.rb
+++ login_spec.rb
@@ -1,9 +1,11 @@
 require 'selenium-webdriver'
+require_relative 'login' #used relative because this is in the same directory
 
-describe 'Login' do
+describe 'Login' do 
 
 	before(:each) do
 		@driver = Selenium::WebDriver.for :firefox
+		@login = Login.new(@driver)
 	end
 
 	after(:each) do
@@ -11,14 +13,8 @@ describe 'Login' do
 	end
 
 	it 'succeeded' do
-		@driver.get 'http://the-internet.herokuapp.com/login'
-		@driver.find_element(id: 'username').send_keys 'tomsmith'
-		@driver.find_element(id: 'password').send_keys 'SuperSecretPassword!'
-		@driver.find_element(css: 'button').click
-		#@driver.find_element(id: 'login').click note that we could also use this option
-
-		expect(@driver.find_element(css: '.flash.success').displayed?).to be_true
+		@login.with 'tomsmith', 'SuperSecretPassword!'
+		expect(@login.success_message_present?).to be_true
 	end
-
 
 end
