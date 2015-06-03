11e908c2fee0d656640e1a4025d0182c5d35260a
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-06-02 23:58:02 -0500

    Adding the exception handling

--- login_spec.rb
+++ login_spec.rb
@@ -17,4 +17,9 @@ describe 'Login' do
 		expect(@login.success_message_present?).to be_true
 	end
 
+	it 'failed' do
+		@login.with 'asdf', 'asdf'
+		expect(@login.failure_message_present?).to be_true
+	end
+
 end
