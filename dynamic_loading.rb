c72ca59ec7ece545c9e187a58ccef73f3f569c60
Evan Niedojadlo <evanniedojadlo@gmail.com>
2015-06-10 23:52:36 -0500

    Adding explicit waits

--- dynamic_loading.rb
+++ dynamic_loading.rb
@@ -4,3 +4,18 @@ class DynamicLoading < Base
 
 	START_BUTTON = { css: '#start button' }
 	FINISH_TEXT = { id: 'finish' }
+
+	def initialize(driver)
+		@driver = driver
+		visit 'http://the-internet.herokuapp.com/dynamic_loading/2'
+	end
+
+	def start
+		click START_BUTTON
+	end
+
+	def finish_text_present?
+		wait_for(6) { displayed? FINISH_TEXT }
+	end
+
+end
